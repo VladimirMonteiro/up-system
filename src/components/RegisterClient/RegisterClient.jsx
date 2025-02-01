ecb9b782319d0f11cde6f798ab06084564fbef3a
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2025-02-01 13:40:54 -0300

    feat: consult cnpj receita federal

--- src/components/RegisterClient/RegisterClient.jsx
+++ src/components/RegisterClient/RegisterClient.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import styles from './RegisterClient.module.css'
+import styles from './RegisterClient.module.css';
 import api from "../../utils/api";
 
 const RegisterClient = () => {
     const [selectedForm, setSelectedForm] = useState("pf"); // "pf" para Pessoa Física e "pj" para Pessoa Jurídica
-    const [name, setName] = useState("")
-    const [cpf, setCpf] = useState("")
-    const [phones, setPhones] = useState(undefined)
-    const [errors, setErrors] = useState(null)
-    const [sucess, setSuccess] = useState(null)
+    const [name, setName] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [phones, setPhones] = useState(undefined);
+    const [errors, setErrors] = useState(null);
+    const [sucess, setSuccess] = useState(null);
     const [addresses, setAddresses] = useState({
         cep: "",
         street: "",
@@ -19,17 +19,15 @@ const RegisterClient = () => {
         state: "",
     });
 
-    const [stateRegistration, setStateRegistration] = useState("")
-    const [municipalRegistration, setMunicipalRegistration] = useState("")
-    const [fantasyName, setFantasyName] = useState("")
-    const [socialReason, setSocialReason] = useState("")
-    const [cnpj, setCnpj] = useState("")
-
-
+    const [stateRegistration, setStateRegistration] = useState("");
+    const [municipalRegistration, setMunicipalRegistration] = useState("");
+    const [fantasyName, setFantasyName] = useState("");
+    const [socialReason, setSocialReason] = useState("");
+    const [cnpj, setCnpj] = useState("");
 
     const handleFormSwitch = (formType) => {
         setSelectedForm(formType);
-        console.log(formType)
+        console.log(formType);
     };
 
     const handleCepChange = async (e) => {
@@ -64,31 +62,64 @@ const RegisterClient = () => {
         const { name, value } = e.target;
         setAddresses({ ...addresses, [name]: value });
     };
+ 
 
+    const handleCnpjChange = async (e) => {
+        const cnpjValue = e.target.value.replace(/\D/g, ""); // Remove caracteres não numéricos
+        setCnpj(cnpjValue);
+    
+        if (cnpjValue.length === 14) {
+            try {
+                const response = await api.get(`http://localhost:8080/clients/consultaCnpjReceitaWs/${cnpjValue}`);
+                const data = response.data
+    
+                console.log(response.data)
+            
+    
+                setName(data.nome || "")
+                setPhones(data.telefone || "")
+                setFantasyName(data.fantasia || "")
+            
+    
+                // Preencher os dados de endereço
+                setAddresses({
+                    cep: data.cep || "",
+                    street: data.logradouro || "",
+                    number: data.numero || "",
+                    complement: data.complemento || "",
+                    neighborhood: data.bairro || "",
+                    city: data.municipio || "",
+                    state: data.uf || "",
+                });
+    
+            } catch (error) {
+                console.error("Erro ao buscar CNPJ:", error);
+                alert("Erro ao buscar o CNPJ. Tente novamente.");
+            }
+        }
+    };
+    
+    
     const handleSubmit = async (e) => {
-        e.preventDefault()
-
-
+        e.preventDefault();
 
         const newClientFs = {
             name,
             cpf,
             phones: phones != null ? [phones] : null,
             addresses: [addresses]
-
-        }
+        };
 
         try {
+            if (selectedForm === "pf") {
+                const response = await api.post(`clients/createFs`, newClientFs);
+                console.log(response.data);
+                setSuccess(response.data.message);
 
-            if (selectedForm == "pf") {
-                const response = await api.post(`clients/createFs`, newClientFs)
-                console.log(response.data)
-                setSuccess(response.data.message)
-
-                setName("")
-                setCpf("")
-                setPhones("")
-                setErrors(null)
+                setName("");
+                setCpf("");
+                setPhones("");
+                setErrors(null);
 
                 const emptyAddress = Object.keys(addresses).reduce((acc, key) => {
                     acc[key] = ""; // Define cada atributo como ""
@@ -97,13 +128,9 @@ const RegisterClient = () => {
                 setAddresses(emptyAddress);
 
                 setTimeout(() => {
-                    setSuccess(null)
-
-                }, 3000)
-
+                    setSuccess(null);
+                }, 3000);
             } else {
-
-
                 const newClientPj = {
                     name,
                     cnpj,
@@ -113,21 +140,20 @@ const RegisterClient = () => {
                     fantasyName,
                     stateRegistration,
                     municipalRegistration
+                };
 
-                }
+                const response = await api.post(`clients/createPj`, newClientPj);
+                console.log(response.data);
+                setSuccess(response.data.message);
 
-                const response = await api.post(`clients/createPj`, newClientPj)
-                console.log(response.data)
-                setSuccess(response.data.message)
-
-                setName("")
-                setCnpj("")
-                setPhones("")
-                setSocialReason("")
-                setFantasyName("")
-                setStateRegistration("")
-                setMunicipalRegistration("")
-                setErrors(null)
+                setName("");
+                setCnpj("");
+                setPhones("");
+                setSocialReason("");
+                setFantasyName("");
+                setStateRegistration("");
+                setMunicipalRegistration("");
+                setErrors(null);
 
                 const emptyAddress = Object.keys(addresses).reduce((acc, key) => {
                     acc[key] = ""; // Define cada atributo como ""
@@ -136,28 +162,17 @@ const RegisterClient = () => {
                 setAddresses(emptyAddress);
 
                 setTimeout(() => {
-                    setSuccess(null)
-
-                }, 3000)
-
-
-
+                    setSuccess(null);
+                }, 3000);
             }
-
-
-
-
         } catch (error) {
-            setErrors(error.response.data.errors)
+            setErrors(error.response.data.errors);
         }
-
-        
-    }
+    };
 
     return (
         <div>
             <h1>Cadastro de Cliente</h1>
-            {/* Botões para alternar os formulários */}
             <div style={{ marginBottom: "20px" }}>
                 <button
                     onClick={() => handleFormSwitch("pf")}
@@ -186,7 +201,7 @@ const RegisterClient = () => {
                 </button>
             </div>
 
-            {sucess && <p style={{ color: "#28a745", textAlign: "center" }}> {sucess}</p>}
+            {sucess && <p style={{ color: "#28a745", textAlign: "center" }}>{sucess}</p>}
             {errors && errors.length > 0 && (
                 <p style={{ color: "red", textAlign: "center" }}>{errors.filter(error => error.includes("Cliente já"))}</p>)}
 
@@ -194,7 +209,6 @@ const RegisterClient = () => {
             {selectedForm === "pf" && (
                 <form className={styles.formContainer} onSubmit={handleSubmit}>
                     <div className={styles.form}>
-
                         <div className={styles.inputContainer}>
                             <label htmlFor="name">Nome</label>
                             <input type="text" name="name" id="name" onChange={e => setName(e.target.value)} value={name} />
@@ -218,7 +232,6 @@ const RegisterClient = () => {
                             <input type="text" name="cep" id="cep" value={addresses.cep} onChange={handleCepChange} />
                             {errors && errors.length > 0 && (
                                 <p style={{ color: "red" }}>{errors.filter(error => error.includes("CEP"))}</p>)}
-
                         </div>
                         <div className={styles.inputContainer}>
                             <label htmlFor="street">Endereço</label>
@@ -260,7 +273,6 @@ const RegisterClient = () => {
                             <input type="submit" value="Cadastrar" />
                         </div>
                     </div>
-
                 </form>
             )}
 
@@ -268,30 +280,33 @@ const RegisterClient = () => {
             {selectedForm === "pj" && (
                 <form className={styles.formContainer} onSubmit={handleSubmit}>
                     <div className={styles.form}>
-
                         <div className={styles.inputContainer}>
                             <label htmlFor="name">Nome</label>
-                            <input type="text" name="name" id="name" onChange={e => setName(e.target.value)} value={name} />
+                            <input type="text" name="name" id="name" onChange={e => setName(e.target.value)} value={name || ""} />
                             {errors && errors.length > 0 && (
                                 <p style={{ color: "red" }}>{errors.filter(error => error.includes("nome"))}</p>)}
                         </div>
 
                         <div className={styles.inputContainer}>
                             <label htmlFor="socialReason">Razão Social</label>
-                            <input type="text" name="socialReason" id="socialReason" onChange={e => setSocialReason(e.target.value)} value={socialReason} />
-                            {errors && errors.length > 0 && (
-                                <p style={{ color: "red" }}>{errors.filter(error => error.includes("razão social"))}</p>)}
+                            <input type="text" name="socialReason" id="socialReason" onChange={e => setSocialReason(e.target.value)} value={socialReason || ""} placeholder="Opcional"/>
                         </div>
                         <div className={styles.inputContainer}>
                             <label htmlFor="cnpj">CNPJ</label>
-                            <input type="text" name="cnpj" id="cnpj" onChange={e => setCnpj(e.target.value)} value={cnpj} />
+                            <input
+                                type="text"
+                                name="cnpj"
+                                id="cnpj"
+                                onChange={handleCnpjChange}
+                                value={cnpj}
+                                maxLength={"14"}
+                            />
                             {errors && errors.length > 0 && (
                                 <p style={{ color: "red" }}>{errors.filter(error => error.includes("CNPJ"))}</p>)}
-
                         </div>
                         <div className={styles.inputContainer}>
                             <label htmlFor="phones">Telefone</label>
-                            <input type="text" name="phones" id="phones" onChange={e => setPhones(e.target.value)} value={phones} />
+                            <input type="text" name="phones" id="phones" onChange={e => setPhones(e.target.value)} value={phones || ""} />
                             {errors && errors.length > 0 && (
                                 <p style={{ color: "red" }}>{errors.filter(error => error.includes("telefone"))}</p>)}
                         </div>
@@ -300,26 +315,19 @@ const RegisterClient = () => {
                             <input type="text" name="cep" id="cep" value={addresses.cep} onChange={handleCepChange} />
                             {errors && errors.length > 0 && (
                                 <p style={{ color: "red" }}>{errors.filter(error => error.includes("CEP"))}</p>)}
-
                         </div>
                         <div className={styles.inputContainer}>
                             <label htmlFor="municipalRegistration">Registro Municipal</label>
-                            <input type="text" name="municipalRegistration" id="municipalRegistration" onChange={e => setMunicipalRegistration(e.target.value)} value={municipalRegistration} />
-                            {errors && errors.length > 0 && (
-                                <p style={{ color: "red" }}>{errors.filter(error => error.includes("O registro do município"))}</p>)}
+                            <input type="text" name="municipalRegistration" id="municipalRegistration" onChange={e => setMunicipalRegistration(e.target.value)} value={municipalRegistration || ""} placeholder="Opcional" />
                         </div>
                         <div className={styles.inputContainer}>
                             <label htmlFor="stateRegistration">Registro Estadual</label>
-                            <input type="text" name="stateRegistration" id="stateRegistration" onChange={e => setStateRegistration(e.target.value)} value={stateRegistration} />
-                            {errors && errors.length > 0 && (
-                                <p style={{ color: "red" }}>{errors.filter(error => error.includes("O Registro do estado"))}</p>)}
-                            
+                            <input type="text" name="stateRegistration" id="stateRegistration" onChange={e => setStateRegistration(e.target.value)} value={stateRegistration || ""} placeholder="Opcional" />
                         </div>
                         <div className={styles.inputContainer}>
                             <label htmlFor="fantasyName">Nome fantasia</label>
-                            <input type="text" name="fantasyName" id="fantasyName" onChange={e => setFantasyName(e.target.value)} value={fantasyName} />
-                            {errors && errors.length > 0 && (
-                                <p style={{ color: "red" }}>{errors.filter(error => error.includes("fantasia"))}</p>)}
+                            <input type="text" name="fantasyName" id="fantasyName" onChange={e => setFantasyName(e.target.value)} value={fantasyName || ""} placeholder="Opcional" />
+                            
                         </div>
                         <div className={styles.inputContainer}>
                             <label htmlFor="street">Endereço</label>
@@ -329,29 +337,29 @@ const RegisterClient = () => {
                         </div>
                         <div className={styles.inputContainer}>
                             <label htmlFor="number">nº</label>
-                            <input type="text" name="number" id="number" value={addresses.number} onChange={handleInputChange} />
+                            <input type="text" name="number" id="number" value={addresses.number || ""} onChange={handleInputChange} />
                             {errors && errors.length > 0 && (
                                 <p style={{ color: "red" }}>{errors.filter(error => error.includes("número"))}</p>)}
                         </div>
                         <div className={styles.inputContainer}>
                             <label htmlFor="complement">Complemento</label>
-                            <input type="text" name="complement" id="complement" value={addresses.complement} onChange={handleInputChange} placeholder="Opcional" />
+                            <input type="text" name="complement" id="complement" value={addresses.complement || ""} onChange={handleInputChange} placeholder="Opcional" />
                         </div>
                         <div className={styles.inputContainer}>
                             <label htmlFor="neighborhood">Bairro</label>
-                            <input type="text" name="neighborhood" id="neighborhood" value={addresses.neighborhood} onChange={handleInputChange} />
+                            <input type="text" name="neighborhood" id="neighborhood" value={addresses.neighborhood || ""} onChange={handleInputChange} />
                             {errors && errors.length > 0 && (
                                 <p style={{ color: "red" }}>{errors.filter(error => error.includes("bairro"))}</p>)}
                         </div>
                         <div className={styles.inputContainer}>
                             <label htmlFor="city">Cidade</label>
-                            <input type="text" name="city" id="city" value={addresses.city} onChange={handleInputChange} />
+                            <input type="text" name="city" id="city" value={addresses.city || ""} onChange={handleInputChange} />
                             {errors && errors.length > 0 && (
                                 <p style={{ color: "red" }}>{errors.filter(error => error.includes("cidade"))}</p>)}
                         </div>
                         <div className={styles.inputContainer}>
                             <label htmlFor="state">Estado</label>
-                            <input type="text" name="state" id="state" value={addresses.state} onChange={handleInputChange} />
+                            <input type="text" name="state" id="state" value={addresses.state || ""} onChange={handleInputChange} />
                             {errors && errors.length > 0 && (
                                 <p style={{ color: "red" }}>{errors.filter(error => error.includes("estado"))}</p>)}
                         </div>
@@ -359,9 +367,7 @@ const RegisterClient = () => {
                         <div className={styles.inputContainer} style={{ marginTop: "35px" }}>
                             <input type="submit" value="Cadastrar" />
                         </div>
-
                     </div>
-
                 </form>
             )}
         </div>
