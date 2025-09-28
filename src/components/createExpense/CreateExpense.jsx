0ce11410b54dd0b93e3d472f5ffeb748b5b69bee
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-09-28 18:36:26 -0300

    Fix: resolve import of createExpense to build

--- src/components/createExpense/CreateExpense.jsx
+++ src/components/createExpense/CreateExpense.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import styles from "./CreateExpense.module.css";
+import styles from "./createExpense.module.css";
 import { handlePriceChange } from "../../utils/handlePriceChange";
 
 const CreateExpense = ({ handleCreateExpense, errors }) => {
